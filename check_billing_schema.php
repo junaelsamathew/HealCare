a140f135656c1ccaba19a211bb2b575d70cf78a0
Juna Elsa Mathew <junaelsamathew@gmail.com>
2026-02-07 21:31:06 +0530

    updated content

---
+++ check_billing_schema.php
@@ -0,0 +1,15 @@
+<?php
+include 'includes/db_connect.php';
+$tables = ['wards', 'rooms', 'admissions', 'billing', 'pharmacy_stock', 'lab_tests'];
+foreach($tables as $t) {
+    echo "--- $t ---\n";
+    $res = $conn->query("DESCRIBE $t");
+    if($res) {
+        while($row = $res->fetch_assoc()) {
+            echo $row['Field'] . " - " . $row['Type'] . "\n";
+        }
+    } else {
+        echo "Error: " . $conn->error . "\n";
+    }
+}
+?>
